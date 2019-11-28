d7e7d1c1f012c705bdad71706cf397a01b6e68f5
Prawee Wongsa <prawee@hotmail.com>
2019-11-28 23:39:02 +0700

    update default template of screen

---
+++ src/screens/SignupScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, StyleSheet, Text } from 'react-native'
+
+const SignupScreen = () => {
+  return (
+    <Text style={{ fontSize: 48 }}>
+      Signup Screen
+    </Text>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export default SignupScreen
