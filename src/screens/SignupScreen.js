92f2511de40c7f152483f80e667f8f73a79c7b46
Prawee Wongsa <prawee@hotmail.com>
2019-11-29 00:42:11 +0700

    testing navigation flow

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,11 +1,19 @@
 import React from 'react'
-import { View, StyleSheet, Text } from 'react-native'
+import { View, StyleSheet, Text, Button } from 'react-native'
 
-const SignupScreen = () => {
+const SignupScreen = ({ navigation }) => {
   return (
-    <Text style={{ fontSize: 48 }}>
-      Signup Screen
-    </Text>
+    <>
+      <Text style={{ fontSize: 48 }}>
+        Signup Screen
+      </Text>
+      <Button 
+        title="Go to Signin" 
+        onPress={() => navigation.navigate('Signin')} />
+      <Button 
+        title="Go to main flow" 
+        onPress={() => navigation.navigate('mainFlow')} />
+    </>
   )
 }
 
