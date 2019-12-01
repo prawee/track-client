df64aabf068f9b230a060638ce8421865d300c39
Prawee Wongsa <prawee@hotmail.com>
2019-12-02 03:35:24 +0700

    when signup is ok then dispatch and set to async storage

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,3 +1,4 @@
+import { AsyncStorage } from 'react-native'
 import createDataContext from './createDataContext'
 import trackerApi from '../api/tracker'
 
@@ -5,6 +6,8 @@ const authReducer = (state, action) => {
   switch(action.type) {
     case 'add_error':
       return { ...state, errorMessage: action.payload }
+    case 'signup':
+      return { errorMessage: '', token: action.payload }
     default:
       return state
   }
@@ -19,7 +22,8 @@ const signup = (dispatch) => {
     try {
       const response = await trackerApi.post('/signup', { email, password })
       console.log('signup(ok) => ', response.data)
-
+      await AsyncStorage.setItem('token', response.data.token)
+      dispatch({ type: 'signup', payload: response.data.token })
     } catch(err) {
       console.log('signup(err) => ', err.response.data)
       dispatch({ type: 'add_error', payload: 'Something went wrong with sign up' })
@@ -44,5 +48,5 @@ const signout = (dispatch) => {
 export const { Provider, Context } = createDataContext(
   authReducer,
   { signin, signout, signup },
-  { isSignedIn: false, errorMessage: '' }
+  { token: null, errorMessage: '' }
 )
