eba93e923cdbb1094c85027188fb863a10945904
Prawee Wongsa <prawee@hotmail.com>
2019-12-02 05:05:47 +0700

    update signin func and change name of reducer case

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -7,7 +7,7 @@ const authReducer = (state, action) => {
   switch(action.type) {
     case 'add_error':
       return { ...state, errorMessage: action.payload }
-    case 'signup':
+    case 'signin':
       return { errorMessage: '', token: action.payload }
     default:
       return state
@@ -23,7 +23,7 @@ const signup = (dispatch) => async ({ email, password }) => {
     const response = await trackerApi.post('/signup', { email, password })
     //console.log('signup(ok) => ', response.data)
     await AsyncStorage.setItem('token', response.data.token)
-    dispatch({ type: 'signup', payload: response.data.token })
+    dispatch({ type: 'signin', payload: response.data.token })
 
     navigate('TrackList')
   } catch(err) {
@@ -32,11 +32,17 @@ const signup = (dispatch) => async ({ email, password }) => {
   }
 }
 
-const signin = (dispatch) => {
-  return ({ email, password }) => {
-    // Try to signin
-    // Handle sucess by update state
-    // Handle failure by showing error message (somehow)
+const signin = (dispatch) => async ({ email, password }) => {
+  // Try to signin
+  // Handle sucess by update state
+  // Handle failure by showing error message (somehow)
+  try {
+    const response = await trackerApi.post('/signin', { email, password })
+    await AsyncStorage.setItem('token', response.data.token)
+    dispatch({ type: 'signin', payload: response.data.token })
+    navigate('TrackList')
+  } catch(err) {
+    dispatch({ type: 'add_error', payload: 'Something went wrong with sign in' })
   }
 }
 
