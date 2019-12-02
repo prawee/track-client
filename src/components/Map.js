550e89fd0fbfcd00be1ad424cfebaa3cdf34befe
Prawee Wongsa <prawee@hotmail.com>
2019-12-03 02:16:52 +0700

    update map with maps and styling

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Text, StyleSheet } from 'react-native'
+import MapView from 'react-native-maps'
 
 const Map = () => {
   return (
-    <Text>
-      I am a map
-    </Text>
+    <MapView style={styles.map} />
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  map: {
+    height: 300,
+  }
+})
 
 export default Map
