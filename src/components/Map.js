90dd96184b9a42a487e5a06085249e20779604bd
Prawee Wongsa <prawee@hotmail.com>
2019-12-03 02:55:34 +0700

    create simple points to draw on polyline

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,6 +3,21 @@ import { Text, StyleSheet } from 'react-native'
 import MapView, { Polyline } from 'react-native-maps'
 
 const Map = () => {
+  let points = []
+  for (let i = 0; i < 20; i++) {
+    if (i % 2 === 0) {
+      points.push({
+        latitude: 37.33233 + i * 0.001,
+        longitude: -122.03121 + i * 0.001
+      })
+    } else {
+      points.push({
+        latitude: 37.33233 - i * 0.002,
+        longitude: -122.03121 + i * 0.001
+      })
+    }
+  }
+
   return (
     <MapView 
       style={styles.map}
@@ -13,7 +28,7 @@ const Map = () => {
         longitudeDelta: 0.01
       }}
     >
-      <Polyline />
+      <Polyline coordinates={points} />
     </MapView>
   )
 }
