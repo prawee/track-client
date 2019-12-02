04c7e353a9d739f378af763a7370e5a95a66e5b8
Prawee Wongsa <prawee@hotmail.com>
2019-12-03 03:08:53 +0700

    create startWatching helper for request permission

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { StyleSheet } from 'react-native'
 import { Text } from 'react-native-elements'
 import { SafeAreaView } from 'react-navigation'
+import { requestPermissionAsync } from 'expo-location'
 import Map from '../components/Map'
 
 const TrackCreateScreen = () => {
+  const [err, setErr] = useState(null)
+
+  const startWatching = async () => {
+    try {
+      await requestPermissionAsync()
+    } catch(e) {
+      setErr(e)
+    }
+  }
+
   return (
     <SafeAreaView forceInset={{ top: 'always' }}>
       <Text h3>Create a Track</Text>
       <Map />
+      {err ? <Text>Please enable location services</Text> : null}
     </SafeAreaView>
   )
 }
