7aad30dc48a613595105b8fbf59eae318ad3d3aa
Prawee Wongsa <prawee@hotmail.com>
2019-12-03 02:20:20 +0700

    initial region point with lat lng

--- src/components/Map.js
+++ src/components/Map.js
@@ -4,7 +4,15 @@ import MapView from 'react-native-maps'
 
 const Map = () => {
   return (
-    <MapView style={styles.map} />
+    <MapView 
+      style={styles.map}
+      initialRegion={{
+        latitude: 37.33233,
+        longitude: -122.03121,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01
+      }}
+    />
   )
 }
 
