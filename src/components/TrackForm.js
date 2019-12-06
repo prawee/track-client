38c780b1ee7e6f3afd9cd453e76fb7d079d1c7f1
Prawee Wongsa <prawee@hotmail.com>
2019-12-07 03:18:10 +0700

    update track form with spacer

--- src/components/TrackForm.js
+++ src/components/TrackForm.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Input,  Button } from 'react-native-elements'
 import { StyleSheet } from 'react-native'
+import Spacer from './Spacer'
 
 const TrackForm = () => {
   return (
     <>
-      <Input />
+      <Spacer>
+        <Input placeholder="Enter name" />
+      </Spacer>
       <Button title="Start Recording" />
     </>
   )
