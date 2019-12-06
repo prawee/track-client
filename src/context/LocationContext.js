9bfcf418b60eab3c20107faff0395c1694ecc842
Prawee Wongsa <prawee@hotmail.com>
2019-12-07 03:21:21 +0700

    update startRecording func and reducer

--- src/context/LocationContext.js
+++ src/context/LocationContext.js
@@ -4,12 +4,16 @@ const locationReducer = (state, action) => {
   switch(action.type) {
     case 'add_current_location':
       return { ...state, currentLocation: action.payload }
+    case 'start_recording':
+      return { ...state, recording: true }
     default:
       return state
   }
 }
 
-const startRecording = dispatch => () => {}
+const startRecording = dispatch => () => {
+  dispatch({ type: 'start_recording' })
+}
 
 const stopRecording = dispatch => () => {}
 
