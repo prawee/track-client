19042d32bbfdaf6d0835ac0f9e7519a3b12ec163
Prawee Wongsa <prawee@hotmail.com>
2019-12-07 00:41:28 +0700

    disable point on map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,22 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Text, StyleSheet } from 'react-native'
 import MapView, { Polyline } from 'react-native-maps'
+import { Context as LocationContext } from '../context/LocationContext'
 
 const Map = () => {
-  let points = []
-  for (let i = 0; i < 20; i++) {
-    if (i % 2 === 0) {
-      points.push({
-        latitude: 37.33233 + i * 0.001,
-        longitude: -122.03121 + i * 0.001
-      })
-    } else {
-      points.push({
-        latitude: 37.33233 - i * 0.002,
-        longitude: -122.03121 + i * 0.001
-      })
-    }
-  }
+  // let points = []
+  // for (let i = 0; i < 20; i++) {
+  //   if (i % 2 === 0) {
+  //     points.push({
+  //       latitude: 37.33233 + i * 0.001,
+  //       longitude: -122.03121 + i * 0.001
+  //     })
+  //   } else {
+  //     points.push({
+  //       latitude: 37.33233 - i * 0.002,
+  //       longitude: -122.03121 + i * 0.001
+  //     })
+  //   }
+  // }
 
   return (
     <MapView 
@@ -28,7 +29,7 @@ const Map = () => {
         longitudeDelta: 0.01
       }}
     >
-      <Polyline coordinates={points} />
+      {/* <Polyline coordinates={points} /> */}
     </MapView>
   )
 }
