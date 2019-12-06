bc5ca2fea34693e4cf28ee1847dce79d02aaf2a6
Prawee Wongsa <prawee@hotmail.com>
2019-12-07 02:14:13 +0700

    using context to update live position

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import { Text, StyleSheet } from 'react-native'
+import { Text, StyleSheet, ActivityIndicator } from 'react-native'
 import MapView, { Polyline } from 'react-native-maps'
 import { Context as LocationContext } from '../context/LocationContext'
 
@@ -18,13 +18,24 @@ const Map = () => {
   //     })
   //   }
   // }
+  const { state: { currentLocation } } = useContext(LocationContext)
+
+  if (!currentLocation) {
+    return <ActivityIndicator size="large" style={{ marginTop: 200 }} />
+  }
 
   return (
     <MapView 
       style={styles.map}
       initialRegion={{
-        latitude: 37.33233,
-        longitude: -122.03121,
+        //latitude: 37.33233,
+        //longitude: -122.03121,
+        ...currentLocation.coords,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01
+      }}
+      region={{
+        ...currentLocation.coords,
         latitudeDelta: 0.01,
         longitudeDelta: 0.01
       }}
