da0f0b6453305d142b9ff64aa4d0b35c1be4ed1b
Prawee Wongsa <prawee@hotmail.com>
2019-12-07 18:29:20 +0700

    draw ployline on map

--- src/components/Map.js
+++ src/components/Map.js
@@ -18,7 +18,7 @@ const Map = () => {
   //     })
   //   }
   // }
-  const { state: { currentLocation } } = useContext(LocationContext)
+  const { state: { currentLocation, locations } } = useContext(LocationContext)
 
   if (!currentLocation) {
     return <ActivityIndicator size="large" style={{ marginTop: 200 }} />
@@ -40,13 +40,13 @@ const Map = () => {
         longitudeDelta: 0.01
       }}
     >
-      {/* <Polyline coordinates={points} /> */}
       <Circle
         center={currentLocation.coords}
         radius={30}
         strokeColor="rgba(158, 158, 255, 1.0)"
         fillColor="rgba(158, 158, 255, 0.3)"
       />
+      <Polyline coordinates={locations.map(loc => loc.coords)} />
     </MapView>
   )
 }
