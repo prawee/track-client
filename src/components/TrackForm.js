b7097d04145387c509d0fb3e48cd97500d6ad4be
Prawee Wongsa <prawee@hotmail.com>
2019-12-07 11:39:06 +0700

    update button for start and stop record

--- src/components/TrackForm.js
+++ src/components/TrackForm.js
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Input,  Button } from 'react-native-elements'
 import { StyleSheet } from 'react-native'
 import Spacer from './Spacer'
+import { Context as LocationContext } from '../context/LocationContext'
 
 const TrackForm = () => {
+  const { 
+    state: { name, recording, locations },
+    startRecording, 
+    stopRecording, 
+    changeName 
+  } = useContext(LocationContext)
+
+  console.log(locations)
+
   return (
     <>
       <Spacer>
-        <Input placeholder="Enter name" />
+        <Input 
+          value={name}
+          placeholder="Enter name"
+          onChangeText={changeName}
+        />
       </Spacer>
-      <Button title="Start Recording" />
+      {recording ? (
+          <Button title="Stop Recording" onPress={stopRecording} /> 
+        ) : (
+          <Button title="Start Recording" onPress={startRecording} />
+        )
+      }
     </>
   )
 }
