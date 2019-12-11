6f3667796c44f5d4e6a22ad0491999efbb2ea79b
Prawee Wongsa <prawee@hotmail.com>
2019-12-11 17:51:25 +0700

    using map view and polyline

--- src/screens/TrackDetailScreen.js
+++ src/screens/TrackDetailScreen.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import { View, StyleSheet, Text } from 'react-native'
 import { Context as TrackContext } from '../context/TrackContext'
+import MapView, { Polyline } from 'react-native-maps'
 
 const TrackDetailScreen = ({ navigation }) => {
   const { state } = useContext(TrackContext)
@@ -10,11 +11,23 @@ const TrackDetailScreen = ({ navigation }) => {
   // console.log('state ', state)
 
   const track = state.find(t => t._id === _id)
+  const initialCoords = track.locations[0].coords
 
   return (
-    <Text style={{ fontSize: 48 }}>
-      {track.name}
-    </Text>
+    <>
+      <Text style={{ fontSize: 48 }}>
+        {track.name}
+      </Text>
+      <MapView
+        initialRegion={{
+          longitudeDelta: 0.01,
+          latitudeDelta: 0.01,
+          ...initialCoords
+        }}
+      >
+        <Polyline coordinates={track.locations.map(loc => loc.coords)} />
+      </MapView>
+    </>
   )
 }
 
