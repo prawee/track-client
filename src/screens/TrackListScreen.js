114dcb409f9c62dcd136ec88f0e166449a46d360
Prawee Wongsa <prawee@hotmail.com>
2019-12-11 17:24:46 +0700

    using NavigationEvents

--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { View, StyleSheet, Text, Button } from 'react-native'
+import { NavigationEvents } from 'react-navigation'
 
 const TrackListScreen = ({ navigation }) => {
   return (
     <>
+      <NavigationEvents onWillFocus={() => console.log('hi there!')} />
       <Text style={{ fontSize: 48 }}>
         TrackList Screen
       </Text>
