46fce1728e91958990e591b57c63e7cda77d1215
Prawee Wongsa <prawee@hotmail.com>
2019-12-11 23:58:40 +0700

    update baseURL

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { AsyncStorage } from 'react-native'
 
 const instance = axios.create({
-  baseURL: 'http://352d6fe6.ngrok.io'
+  baseURL: 'http://5e93198f.ngrok.io'
 })
 
 instance.interceptors.request.use(
