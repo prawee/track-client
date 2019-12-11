2ad0a51912bc253578718d2308b1cfc05b4705f8
Prawee Wongsa <prawee@hotmail.com>
2019-12-11 17:45:37 +0700

    using context and filter it with params

--- src/screens/TrackDetailScreen.js
+++ src/screens/TrackDetailScreen.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { View, StyleSheet, Text } from 'react-native'
+import { Context as TrackContext } from '../context/TrackContext'
 
-const TrackDetailScreen = () => {
+const TrackDetailScreen = ({ navigation }) => {
+  const { state } = useContext(TrackContext)
+  const _id = navigation.getParam('_id')
+
+  // console.log('id ', _id)
+  // console.log('state ', state)
+
+  const track = state.find(t => t._id === _id)
+
   return (
     <Text style={{ fontSize: 48 }}>
-      TrackDetail Screen
+      {track.name}
     </Text>
   )
 }
